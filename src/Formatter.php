016ffc8b6a1e68872963b8dfb8060beace7e4562
Rich Morgan <ionux@users.noreply.github.com>
2017-08-03 11:44:49 -0400

    Update Formatter.php

--- src/Formatter.php
+++ src/Formatter.php
@@ -24,7 +24,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ******************************************************************************/
- 
+
 namespace Weather;
 
 /**
@@ -62,5 +62,14 @@ trait Formatter
             default:
                 throw new \Exception("Cannot convert data to CSV format.");
         }
+    }
+
+    private function formatJSON($rawData)
+    {
+        if (is_array($raw_data)) {
+            return json_encode($rawData, true);
+        }
+
+        throw new \Exception("Cannot convert data to JSON format.");
     }
 }
