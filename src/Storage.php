6775adfb7da70bc817a83727a29154801f807ec9
Rich Morgan <ionux@users.noreply.github.com>
2017-08-03 19:39:01 -0400

    Update Storage.php

--- src/Storage.php
+++ src/Storage.php
@@ -24,7 +24,9 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ******************************************************************************/
+
 namespace Weather;
+
 /**
  * Helper to persist the weather and location data.
  *
