7bffa820abc540b73e99e04982f40a5adc9fc33e
Rich Morgan <ionux@users.noreply.github.com>
2017-08-04 08:43:18 -0400

    Create StorageTest.php

---
+++ tests/StorageTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests;
+
+use \Weather\Communicator;
+use \Weather\Storage;
+use \Weather\Curl;
+use \Weather\Weather;
+
+class StorageTest extends \PHPUnit_Framework_TestCase
+{
+    protected $a;
+    protected $b;
+    protected $mock;
+
+    public function setUp()
+    {
+        // Mock class for the Storage trait.
+        $this->mock = $this->getMockForTrait('\Weather\Storage');
+    }
+    
+    public function storeWeatherDataTest()
+    {
+        $this->mock->storeData()
+    }
+    
+    public function getWeatherDataTest()
+    {
+    
+    }
+}
